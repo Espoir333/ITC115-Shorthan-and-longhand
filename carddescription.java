2abc04522753f8750a1d34ed9eb40462a15775c0
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2019-10-28 17:27:05 -0700

    Create carddescription.java

---
+++ carddescription.java
@@ -0,0 +1,12 @@
+//CardDescription.java - Espoir Makiese
+
+import java.util.*;
+
+public class CardDescription
+{
+	public static void main(String[] args)
+	{
+		Card c1 = new Card("KS");
+		System.out.println("Your card is " + c1.getDescription());
+	}
+}
